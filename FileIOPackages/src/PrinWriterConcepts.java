e9a3c932a31605d3dc359f42bc111d64d07ea417
bikash <bikash@Vkash>
2018-05-06 20:58:27 +0545

    Printwriter added

--- FileIOPackages/src/PrinWriterConcepts.java
+++ FileIOPackages/src/PrinWriterConcepts.java
@@ -1,8 +1,16 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class PrinWriterConcepts {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		PrintWriter pw=new PrintWriter("def.txt");
+		pw.write(100);
+		pw.println(100);
+		pw.print(100);
+		pw.flush();
+		pw.close();
 
 	}
 
